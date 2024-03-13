a20213519f1348bffb0105c03ec2fac4d5b862c3
Romário Lima <devromariolima@gmail.com>
2024-03-13 15:45:06 -0300

    Resolução do desafio 1

--- desafio_1.js
+++ desafio_1.js
@@ -11,7 +11,16 @@
 // Ao final do processamento, qual será o valor da variável SOMA?
 
 
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
 
+while (K < INDICE) {
+  K = K + 1;
+  SOMA = SOMA + K;
+}
+
+console.log("1) Valor da soma dos números de 1 a 13:", SOMA);
 
 
 
