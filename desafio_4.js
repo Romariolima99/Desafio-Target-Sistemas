cdf0062394e9ad757810001a6f6d5c160558bbaa
Romário Lima <devromariolima@gmail.com>
2024-03-13 15:52:12 -0300

    Resolução do desafio 4

--- desafio_4.js
+++ desafio_4.js
@@ -1,5 +1,8 @@
 // 4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
 
-// Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+// Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?//  
 
-//  
+console.log("4) Para descobrir, siga os passos:");
+console.log("1. Ligue o primeiro interruptor por alguns minutos e depois desligue.");
+console.log("2. Ligue o segundo interruptor e, em seguida, entre na sala.");
+console.log("3. O interruptor que estiver ligado corresponderá à lâmpada acesa. O interruptor desligado, porém quente, corresponderá ao segundo interruptor, e o terceiro, ao interruptor frio.")
