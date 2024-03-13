f3f4c857c7477b08c20e68d7efed9cd368adde8a
Romário Lima <devromariolima@gmail.com>
2024-03-13 19:26:23 -0300

    Adição da descrição do desafio 3

--- desafio_3.js
+++ desafio_3.js
@@ -14,8 +14,27 @@
 
 
 console.log("a) 1, 3, 5, 7, 9");
+//Essa sequência esta em progressão aritmética, onde cada número é aumentado em 2 em relação ao anterior. 
+// Portanto, o próximo número seria 7 + 2 = 9.
+
 console.log("b) 2, 4, 8, 16, 32, 64, 128");
+// Essa sequência esta em uma progressão geométrica, onde cada número é multiplicado por 2 em relação ao anterior.
+// Portanto, o próximo número seria 64 * 2 = 128.
+
+
 console.log("c) 0, 1, 4, 9, 16, 25, 36, 49");
+// Essa sequência esta em uma progressão dos quadrados dos números naturais. 
+// Portanto, o próximo número seria 7^2 = 49.
+
 console.log("d) 4, 16, 36, 64, 100");
+// Essa sequência esta em uma progressão dos quadrados dos números pares, começando em 2. 
+// Portanto, o próximo número seria 8^2 = 64.
+
 console.log("e) 1, 1, 2, 3, 5, 8, 13");
+// Essa sequência é a famosa sequência de Fibonacci, onde cada número é a soma dos dois números anteriores. 
+// Portanto, o próximo número seria 8 + 5 = 13.
+
 console.log("f) 2, 10, 12, 16, 17, 18, 19, 20");
+// Essa sequência esta em uma sequência mista, onde alguns números aumentam em 2, outros em 4, e outros em 1. 
+// Portanto, o próximo número seria 19 + 1 = 20.
+
