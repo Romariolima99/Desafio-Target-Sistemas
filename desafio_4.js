a667dcb6edf9631d73292058b39e88712e679e48
Romário Lima <devromariolima@gmail.com>
2024-03-13 23:06:50 -0300

    correção do desafio 4

--- desafio_4.js
+++ desafio_4.js
@@ -2,7 +2,7 @@
 // Seu objetivo é descobrir qual interruptor controla qual lâmpada.
 // Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada? 
 
-console.log("4) Para descobrir, siga os passos:");
-console.log("1. Ligue o primeiro interruptor por alguns minutos e depois desligue.");
-console.log("2. Ligue o segundo interruptor e, em seguida, entre na sala.");
-console.log("3. O interruptor que estiver ligado corresponderá à lâmpada acesa. A lâmpada desligada, porém quente, corresponderá ao primeiro interruptor que foi ligado e depois desligado, e o terceiro interruptor, corresponderá a lâmpada que esta fria.")
+console.log("1) Para descobrir, siga os passos:");
+console.log("2. Ligue o primeiro interruptor por alguns minutos e depois desligue.");
+console.log("4. Ligue o segundo interruptor e, em seguida, entre na sala.");
+console.log("4. O interruptor que estiver ligado corresponderá à lâmpada acesa. A lâmpada desligada, porém quente, corresponderá ao primeiro interruptor que foi ligado e depois desligado, e o terceiro interruptor, corresponderá a lâmpada que esta fria.")
